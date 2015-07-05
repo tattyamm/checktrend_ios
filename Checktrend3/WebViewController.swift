b16497caa3ccac3aa7f86ee3e6ea0aa8ca6c2ad3
tattyamm <entatsu.appli+github@gmail.com>
2015-07-05 17:44:56 +0900

    fix webview output url

--- Checktrend3/WebViewController.swift
+++ Checktrend3/WebViewController.swift
@@ -7,19 +7,26 @@ class WebViewController: UIViewController,UIWebViewDelegate {
 
     var startUrl:String = ""
     var viewTitle = ""
+    let myWebView : UIWebView = UIWebView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = viewTitle  // webのタイトルにするか？
 
-        let myWebView : UIWebView = UIWebView()
         myWebView.delegate = self
         myWebView.frame = self.view.bounds
         self.view.addSubview(myWebView)
         let url: NSURL = NSURL(string: startUrl)!
         let request: NSURLRequest = NSURLRequest(URL: url)
         myWebView.loadRequest(request)
+        
+        //button
+        var settingBtn:UIButton = UIButton.buttonWithType(UIButtonType.Custom) as! UIButton
+        settingBtn.addTarget(self, action: "onClickNavBarButton", forControlEvents: UIControlEvents.TouchUpInside)
+        settingBtn.frame = CGRectMake(0, 0, 24, 24)
+        settingBtn.setImage(UIImage(named: "Info.png"), forState: .Normal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: settingBtn)
     }
 
     func webViewDidFinishLoad(webView: UIWebView) {
@@ -28,6 +35,16 @@ class WebViewController: UIViewController,UIWebViewDelegate {
 
     func webViewDidStartLoad(webView: UIWebView) {
         println("ページ読み込み開始")
+    }
+    
+    func onClickNavBarButton() {
+        let currentUrl : String = (myWebView.request?.URL!.absoluteString)!
+        println("current url : " + currentUrl)
+        
+        let url = NSURL(string: currentUrl)
+        if UIApplication.sharedApplication().canOpenURL(url!){
+            UIApplication.sharedApplication().openURL(url!)
+        }
     }
     
     override func didReceiveMemoryWarning() {
