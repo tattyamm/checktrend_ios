ce61e64551b08741745cd78ba29d9686c05af86c
tattyamm <entatsu.appli+github@gmail.com>
2015-07-09 23:03:05 +0900

    add admob

--- Checktrend3/WebViewController.swift
+++ Checktrend3/WebViewController.swift
@@ -2,18 +2,22 @@
 //  http://kzy52.com/entry/2015/02/20/000838
 
 import UIKit
+import GoogleMobileAds
 
-class WebViewController: UIViewController,UIWebViewDelegate {
+class WebViewController: UIViewController,UIWebViewDelegate, GADBannerViewDelegate {
 
     var startUrl:String = ""
     var viewTitle = ""
     let myWebView : UIWebView = UIWebView()
+    
+    var bannerView: GADBannerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = viewTitle  // webのタイトルにするか？
 
+        //webview
         myWebView.delegate = self
         myWebView.frame = self.view.bounds
         self.view.addSubview(myWebView)
@@ -27,6 +31,26 @@ class WebViewController: UIViewController,UIWebViewDelegate {
         settingBtn.frame = CGRectMake(0, 0, 24, 24)
         settingBtn.setImage(UIImage(named: "Info.png"), forState: .Normal)
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: settingBtn)
+
+        //ad http://www.sirochro.com/note/swift-admob-implementation/
+        let bannerView:GADBannerView = getAdBannerView()
+        self.view.addSubview(bannerView)
+    }
+    
+    private func getAdBannerView() -> GADBannerView {
+        var bannerView: GADBannerView = GADBannerView()
+        bannerView = GADBannerView(adSize:kGADAdSizeBanner)
+        //TODO tabbar分の高さを引く
+        bannerView.frame.origin = CGPointMake(0, self.view.frame.size.height - bannerView.frame.height - 50)
+        bannerView.frame.size = CGSizeMake(self.view.frame.width, bannerView.frame.height)
+        bannerView.adUnitID = "ca-app-pub-5040713306305537/8685162711"
+        bannerView.delegate = self
+        bannerView.rootViewController = self
+        
+        var request:GADRequest = GADRequest()
+        bannerView.loadRequest(request)
+        
+        return bannerView
     }
 
     func webViewDidFinishLoad(webView: UIWebView) {
