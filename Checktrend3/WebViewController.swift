7cdbb6b6b3fe85b514ae5e394340a0684b1d13c1
tattyamm <entatsu.appli+github@gmail.com>
2015-07-11 01:47:27 +0900

    fix optional

--- Checktrend3/WebViewController.swift
+++ Checktrend3/WebViewController.swift
@@ -10,6 +10,7 @@ class WebViewController: UIViewController,UIWebViewDelegate, GADBannerViewDelega
     var viewTitle = ""
     let myWebView : UIWebView = UIWebView()
     let ADMOB_ID = "ca-app-pub-5040713306305537/8685162711"
+    let TABBAR_HEIGHT_DEFAULT : CGFloat = 44.0
     
     var bannerView: GADBannerView!
 
@@ -24,7 +25,7 @@ class WebViewController: UIViewController,UIWebViewDelegate, GADBannerViewDelega
         //webview
         myWebView.delegate = self
         //myWebView.frame = self.view.bounds
-        var navBarHeight = (self.navigationController?.navigationBar.frame.size.height)!
+        var navBarHeight = (self.navigationController?.navigationBar.frame.size.height) ?? TABBAR_HEIGHT_DEFAULT
         myWebView.frame = CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height - navBarHeight)
         self.view.addSubview(myWebView)
         let url: NSURL = NSURL(string: startUrl)!
@@ -47,7 +48,7 @@ class WebViewController: UIViewController,UIWebViewDelegate, GADBannerViewDelega
         var bannerView: GADBannerView = GADBannerView()
         bannerView = GADBannerView(adSize:kGADAdSizeBanner)
 
-        var navBarHeight = (self.navigationController?.navigationBar.frame.size.height)!
+        var navBarHeight = (self.navigationController?.navigationBar.frame.size.height) ?? TABBAR_HEIGHT_DEFAULT
         bannerView.frame.origin = CGPointMake(0, self.view.frame.size.height - bannerView.frame.height - navBarHeight)
         bannerView.frame.size = CGSizeMake(self.view.frame.width, bannerView.frame.height)
         bannerView.adUnitID = ADMOB_ID
